123e8aa9e666008dfc040954d5a5537eb6c593e1
Matt Huston <mhuston@teleflora.com>
2017-03-30 20:25:43 -0600

    ran rsync

---
+++ bang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This will rsync your $HOME , add all to your local git repo, commit and push
+# Copyleft 2017, Matt Huston
+# All rights reserved, all wrongs reversed
+# rsync -avzh /home/mhuston /home/mhuston_home/RSYNC_POWER_OUTAGE/
+# sleep 10
+# REPO=/home/mhuston/mhuston_home
+# pushd $REPO
+# git add . && git commit -a -m "ran rsync" && git push -u origin master
+# popd
+# exit
--- local.backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avzh /DATA/mhuston_home /DATA/home_backup/RSYNC_POWER_OUTAGE/
